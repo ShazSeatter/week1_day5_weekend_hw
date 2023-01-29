88bcc10df3ccad14cd08508c1c1dfef72bf6eb24
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-29 18:10:57 +0000

    answerd optional tests

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -23,27 +23,21 @@ def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
     
 
-def get_pets_by_breed(pet_shop, breed):
-    for pet in pet_shop["pets"]:
-        if pet == breed:
-            return pet
+# def get_pets_by_breed(pet_shop, breed):
+
 
 
 def find_pet_by_name(pet_shop, name):
    for pet_name in pet_shop["pets"]:
     if pet_name["name"] == name:
         return pet_name
-   
-# @unittest.skip("delete this line to run the test")
-# def test_remove_pet_by_name(self):
-#     remove_pet_by_name(self.cc_pet_shop, "Arthur")
-#     pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
-#     self.assertIsNone(pet)
+
+
 
 # def remove_pet_by_name(pet_shop, name):
-
-
-
+#     for pet_name in pet_shop["pets"]["name"]:
+#         if pet_name == name:
+#             pet_shop["name"].remove(name)
 
 
 
@@ -64,21 +58,21 @@ def get_customer_pet_count(customer):
 def add_pet_to_customer(customer, new_pet):
     customer["pets"].append(new_pet)
     
-    
-
-
-# basically this function can be reuseable for other breeds if we want to look up
-# how many (so a count) of that specified breed is in the pet shop 
-
-# print(get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
-
-# print(get_pets_by_breed(self.cc_pet_shop, "Dalmation")
-
 
 # what is the function called?
 # what parameters does it need?
 # what does the function need to return?
 # what does the function need to do?
 
+# --- OPTIONAL section ---
+
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    elif customer["cash"] < new_pet["price"]:
+        return False 
+
+
+# Integration tests section 
 
 
